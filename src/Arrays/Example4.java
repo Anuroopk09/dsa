b101d715dd5a6be03a6b1b1b74246d7bb363f29c
anuroop <anuroopkanade09@gmail.com>
2026-01-04 15:25:00 +0530

    "Added new example 4 and 5"

---
+++ src/Arrays/Example4.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+import java.util.HashSet;
+
+public class Example4 {
+    //remove duplicates from sorted array
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 2, 3, 3, 4, 5, 5};
+        int arr2[] = new int[arr.length];
+
+        int j = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                arr2[j++] = arr[i];
+
+            }
+
+        }
+        arr2[j++] = arr[arr.length - 1];
+
+        for (int k = 0; k < j; k++) {
+            System.out.print(arr2[k] + " ");
+        }
+        System.out.println();
+
+        //to hashset
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int x : arr) {
+            set.add(x);
+        }
+
+        System.out.println(set);
+    }
+
+
+
+}
