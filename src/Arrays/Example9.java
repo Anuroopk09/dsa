334de373443cc3b71e37f6599fb4995f55c3c920
anuroop <anuroopkanade09@gmail.com>
2026-01-06 13:23:27 +0530

    "added example 9"

--- src/Arrays/Example9.java
+++ src/Arrays/Example9.java
@@ -2,6 +2,33 @@ package Arrays;
 
 public class Example9
 {
+    //frequency counter of an array
+    public static void main(String[] args) {
+        int[] arr={2,2,3,3,1,2,2};
+        int size=arr[0];
 
 
+        for (int i =0;i<arr.length;i++)
+        {
+            if (arr[i]>size)
+            {
+                size = arr[i];
+            }
+        }
+        //creating a new hash-array to store the count (occurences of each element)
+        int H[] = new int[size+1];
+        for (int j =0;j<arr.length;j++)
+        {
+            H[arr[j]]++;
+        }
+        int max= 0;
+        for (int i = 0;i<H.length;i++)
+        {
+            if (H[i]>max)
+            {
+                max = i;
+            }
+        }
+        System.out.println(max);
+    }
 }
