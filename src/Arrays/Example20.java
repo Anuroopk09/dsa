f67e8f87f51ad2a6096ca77706bd5c76741354ff
anuroop <anuroopkanade09@gmail.com>
2026-01-13 23:34:45 +0530

    "added example 20"

---
+++ src/Arrays/Example20.java
@@ -0,0 +1,29 @@
+package Arrays;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class Example20
+{
+    //sort all the positives and negatives in an array
+    static void main()
+    {
+        int[] arr = {1, 2, -4, -5};
+        Arrays.sort(arr);
+        int count = 0;
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+
+        for (int i =0;i<arr.length;i++)
+        {
+            if (arr[i]>0)
+            {
+                pos.add(arr[i]);
+            }
+            else
+            {
+                neg.add(arr[i]);
+            }
+        }
+        neg.addAll(pos);
+        System.out.println(neg);
+    }
+}
