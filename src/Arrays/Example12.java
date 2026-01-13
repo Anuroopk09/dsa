72902b8ec1a930f0702958492202fac6f4429710
anuroop <anuroopkanade09@gmail.com>
2026-01-14 00:02:12 +0530

    "added example 12"

--- src/Arrays/Example12.java
+++ src/Arrays/Example12.java
@@ -1,4 +1,37 @@
 package Arrays;
 
-public class Example12 {
+
+public class Example12
+{
+    //maximum sub array sum
+    public static int maxSum(int[] arr)
+    {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+
+        for (int i=0;i<arr.length;i++)
+        {
+            for (int j = i;j<arr.length;j++)
+            {
+                sum = 0;
+                for (int k = i;k<arr.length;k++)
+                {
+                    sum+=arr[k];
+                }
+                if (sum>max)
+                {
+                    max = sum;
+                }
+
+            }
+        }
+        return max;
+    }
+
+    static void main()
+    {
+        int[] arr = {2,3,-8,7,-1,2,3};
+        System.out.println(maxSum(arr));
+    }
+
 }
