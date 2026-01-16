93461ad359c84a66e3e3b98720e8fef4c1f647a2
anuroop <anuroopkanade09@gmail.com>
2026-01-16 07:29:20 +0530

    "added example 21"

--- src/Arrays/Example21.java
+++ src/Arrays/Example21.java
@@ -1,7 +1,26 @@
 package Arrays;
 
+
 public class Example21 {
+
+    //linear search
+    public static int linearSearch(int[] arr)
+    {
+        int target = 1;
+        for (int i =0;i<arr.length;i++)
+        {
+            if (arr[i]==target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
     static void main() {
+        int[] arr ={3,6,5,1,2};
+        System.out.println("Element found at index:"+linearSearch(arr));
+
+
 
     }
 }
