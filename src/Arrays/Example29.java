46ac6d48e65a1c8c9b239a02780eace78c05c2e2
anuroop <anuroopkanade09@gmail.com>
2026-01-31 11:16:09 +0530

    "added example 29"

---
+++ src/Arrays/Example29.java
@@ -0,0 +1,44 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example29 {
+
+    public static List<Integer> sortArr(int[] arr) {
+
+        ArrayList<Integer> positive = new ArrayList<>();
+        ArrayList<Integer> negative = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                positive.add(arr[i]);
+            } else {
+                negative.add(arr[i]);
+            }
+        }
+
+        ArrayList<Integer> newArray = new ArrayList<>();
+
+        int p = 0;
+        int n = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                newArray.add(positive.get(p));
+                p++;
+            } else {
+                newArray.add(negative.get(n));
+                n++;
+            }
+        }
+
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, -3, 2, -1, 3, -2};
+        System.out.println(sortArr(arr));
+    }
+}
