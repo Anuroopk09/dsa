9eb5b42a60fcf0bada5e765432a03c53812bd766
anuroop <anuroopkanade09@gmail.com>
2026-02-02 11:29:53 +0530

    "Added example 30"

---
+++ src/Arrays/Example30.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Example30
+{
+    //majority element in an array(>n/3)
+    public static void main(String[] args)
+    {
+        int[] arr = {1,2,1,1,3,2};
+        Map<Integer, Integer> map= new HashMap<>();
+        for (int i = 0; i < arr.length; i++)
+        {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue()>arr.length/3)
+            {
+                System.out.println(entry.getValue());
+            }
+
+        }
+
+    }
+}
--- src/Arrays/Example6.java
+++ src/Arrays/Example6.java
@@ -7,7 +7,6 @@ public class Example6 {
     public static void main(String[] args) {
         int[] arr = new int[]{3, 6, 0, 1, 0, 5, 0, 3};
         ArrayList<Integer> arr2 = new ArrayList<>();
-        int count = 0;
 
         for(int i = 0; i < arr.length; ++i) {
             if (arr[i] != 0) {
