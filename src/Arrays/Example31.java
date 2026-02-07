16103e5be9b06fed20423a5d36335154ef4fa40d
anuroop <anuroopkanade09@gmail.com>
2026-02-07 11:32:54 +0530

    "added example 31"

---
+++ src/Arrays/Example31.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+public class Example31 {
+    public static void main(String[] args)
+    {
+
+        int[] arr = {6, -2, 2, -8, 1, 7, 4, -10};
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            int sum = 0;
+
+            for (int j = i; j < arr.length; j++)
+            {
+                sum += arr[j];
+
+                if (sum == 0)
+                {
+
+                    for (int k = i; k <= j; k++)
+                    {
+                        System.out.print(arr[k] + " ");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
