5469195ca15b4799d2638893a29f10d4d040bc7d
anuroop <anuroopkanade09@gmail.com>
2026-02-13 11:11:06 +0530

    "added exmaple 38"

---
+++ src/Arrays/Example38.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+public class Example38
+{
+    //find target index of an element
+    public static int findTargetIndex(int[] arr,int target)
+    {
+        int low =0;
+        int high = arr.length-1;
+        int answer = arr.length;
+
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+
+            if (arr[mid]>=target)
+            {
+                answer= mid;
+                high = mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args)
+    {
+        int[] arr = {1,2,4,7};
+        int target = 6;
+        int answer = findTargetIndex(arr,target);
+        System.out.println(answer);
+
+    }
+
+}
