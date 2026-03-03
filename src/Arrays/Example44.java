d65e05418dd3555737dc6176afd137ff3d2da086
anuroop <anuroopkanade09@gmail.com>
2026-03-03 09:04:27 +0530

    added example 44

--- src/Arrays/Example44.java
+++ src/Arrays/Example44.java
@@ -2,17 +2,41 @@ package Arrays;
 
 public class Example44
 {
-    //search element in rotated sorted array linear search
+    //search element in rotated sorted array (binary search)
     static int searchElement(int[] arr, int target)
     {
-        for (int i =0;i<arr.length;i++)
+        int low = 0;
+        int high = arr.length-1;
+        while (low<=high)
         {
-            if (arr[i]==target)
+            int mid = (low+high)/2;
+            if (arr[mid]==target) {
+                return mid;
+            }
+            if (arr[low]<=arr[mid])
             {
-                return i;
+                if (arr[low]<=target &&target<=arr[mid])
+                {
+                    high = mid-1;
+                }
+                else
+                {
+                    low = mid+1;
+                }
+            }
+            else
+            {
+                if(arr[mid]<target && target <=arr[high])
+                {
+                    low = mid+1;
+                }
+                else {
+                    high = mid-1;
+                }
             }
         }
         return -1;
+
     }
     public static void main(String[] args)
     {
