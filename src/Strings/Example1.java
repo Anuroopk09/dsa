8dae64fc74fca00cc21d11a6c55499f4ee39bf1d
anuroop <anuroopkanade09@gmail.com>
2026-03-08 16:52:13 +0530

    added string example 1

---
+++ src/Strings/Example1.java
@@ -0,0 +1,24 @@
+package Strings;
+
+public class Example1
+{
+    static String largeOddNumber(String str1)
+    {
+        for(int i = str1.length()-1; i >= 0; i--)
+        {
+            if((str1.charAt(i) - '0') % 2 == 1)
+            {
+                return str1.substring(0, i+1);
+            }
+        }
+
+        return "";
+    }
+
+    public static void main(String[] args)
+    {
+        String str1 = "5347";
+        String ans = largeOddNumber(str1);
+        System.out.println(ans);
+    }
+}
