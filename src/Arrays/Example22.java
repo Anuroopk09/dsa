b626e9f6a95d77ff0db2b5d5deaa3b054a9b4b1e
anuroop <anuroopkanade09@gmail.com>
2026-03-20 10:58:36 +0530

    modified example 22

--- src/Arrays/Example22.java
+++ src/Arrays/Example22.java
@@ -5,14 +5,14 @@ public class Example22
     //stock buy and sell
     public static int stockBuySell(int[] prices)
     {
-        int maxProfit = 0;
-        for (int i = 0; i < prices.length; i++)
-        {
-            for (int j = i + 1; j < prices.length; j++)
-            {
-                int profit = prices[j] - prices[i];
-                maxProfit = Math.max(maxProfit, profit);
+        int n=prices.length;
+        int maxProfit=0;
+        int bestBuy=prices[0];
+        for(int i=0;i<n;i++){
+            if(prices[i]>bestBuy){
+                maxProfit=Math.max(maxProfit, prices[i]-bestBuy);
             }
+            bestBuy=Math.min(bestBuy,prices[i]);
         }
         return maxProfit;
     }
