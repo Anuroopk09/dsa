7d87dec820ced49b89f598caae89a543d398ab93
anuroop <anuroopkanade09@gmail.com>
2026-04-06 11:07:39 +0530

    added 59

---
+++ src/Arrays/Example59.java
@@ -0,0 +1,34 @@
+package Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example59 {
+    static List<Integer> findDisappearedNumbers(int[] nums)
+    {
+        int n = nums.length;
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int index = Math.abs(nums[i]) - 1;
+
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] nums={4,3,2,7,8,2,3,1};
+        System.out.println(findDisappearedNumbers(nums));
+
+    }
+}
