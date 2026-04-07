588133c5339a1ba1aac2fc36bc5f41c9b69509fe
anuroop <anuroopkanade09@gmail.com>
2026-04-07 10:53:29 +0530

    added 60

---
+++ src/Arrays/Example60.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+import java.util.HashSet;
+
+public class Example60
+{
+    static int distributeCandies(int[] candyType)
+    {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int candy : candyType)
+        {
+                set.add(candy);
+        }
+
+        return Math.min(set.size(), candyType.length / 2);
+    }
+
+    public static void main(String[] args)
+    {
+        int[] nums = {1,1,2,2,3,3};
+        System.out.println(distributeCandies(nums));
+
+    }
+}
