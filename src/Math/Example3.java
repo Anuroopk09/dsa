2f88f914e64986c48629506955b824c2b96c3465
anuroop <anuroopkanade09@gmail.com>
2026-04-12 10:44:50 +0530

    added 3

---
+++ src/Math/Example3.java
@@ -0,0 +1,35 @@
+package Math;
+
+public class Example3
+{
+    static int fib(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+
+        if (n == 1)
+        {
+            return 1;
+        }
+
+        int a = 0;
+        int b = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+        }
+
+    public static void main(String[] args)
+    {
+        int n =5;
+        System.out.println(fib(n));
+
+    }
+}
