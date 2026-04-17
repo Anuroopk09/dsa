58518d9809c88e62bfc2a817f92b548174d8c7e9
anuroop <anuroopkanade09@gmail.com>
2026-04-17 10:37:52 +0530

    added 18

---
+++ src/Strings/Example18.java
@@ -0,0 +1,30 @@
+package Strings;
+
+import java.util.HashSet;
+
+class Example18 {
+    static int lengthOfLongestSubstring(String s)
+    {
+        HashSet<Character> set = new HashSet<>();
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) 
+        {
+            while (set.contains(s.charAt(right)))
+            {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        String str1 = "abcdsbda";
+        System.out.println(lengthOfLongestSubstring(str1));
+    }
+}
