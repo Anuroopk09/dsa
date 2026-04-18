a3f5f69df653e68372a44ef4148207515de30248
anuroop <anuroopkanade09@gmail.com>
2026-04-18 13:15:28 +0530

    modified 63

--- src/Arrays/Example63.java
+++ src/Arrays/Example63.java
@@ -28,7 +28,7 @@ class Example63 {
     }
 
     public static void main(String[] args) {
-        int[] arr = {5,6,4,3,3};
+        int[] arr = {1,2,3};
         System.out.println(permute(arr));
     }
 }
