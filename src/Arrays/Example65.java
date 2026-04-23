305154d5eae2ba1a1578d58ff6757dff496d1b9a
anuroop <anuroopkanade09@gmail.com>
2026-04-23 10:40:05 +0530

    added 65

---
+++ src/Arrays/Example65.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+class Example65 {
+    static void rotate(int[] nums, int k)
+   {
+        int n = nums.length;
+        k = k % n; 
+        for (int i = 0; i < k; i++) 
+        {
+            rotateOnce(nums);
+        }
+    }
+
+    static void rotateOnce(int[] nums)
+    {
+        int last = nums[nums.length - 1];
+        for (int i = nums.length - 1; i > 0; i--) 
+        {
+            nums[i] = nums[i - 1];
+        }
+        nums[0] = last;
+    }
+    static void printArr(int[] nums)
+    {
+        for(int i =0;i<nums.length;i++)
+        {
+            System.out.print(nums[i]+" ");
+        }
+        System.out.println();
+    }
+
+
+}
