6092464643b47d34a9e14d7fec595b7cab709e0a
anuroop <anuroopkanade09@gmail.com>
2026-05-26 15:04:58 +0530

    added 23

--- src/Strings/Example23.java
+++ src/Strings/Example23.java
@@ -3,7 +3,6 @@ package Strings;
 class Example23
 {
     //count the number of special characters(characters which are present in both uppercase and lowercase)
-
     static int numberOfSpecialChars(String word)
     {
         boolean[] lower = new boolean[26];
