eef1991542f330e9f5de66040bdb0191d6f7960a
anuroop <anuroopkanade09@gmail.com>
2026-05-29 10:34:49 +0530

    added 74

---
+++ src/Arrays/Example74.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+class Example74
+{
+    public int minElement(int[] nums)
+    {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) 
+        {
+            int temp = 0;
+            int num = nums[i];
+
+            while (num > 0) 
+            {
+                temp += num % 10;
+                num /= 10;
+            }
+            min = Math.min(min, temp);
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {32,12,34,65,13};
+        Example74 instance = new Example74();
+        int ans = instance.minElement(nums);
+        System.out.println(ans);
+    }
+
+}
