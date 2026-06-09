65ba99242a36c819f05c24d6b836b92ff4f9369d
anuroop <anuroopkanade09@gmail.com>
2026-06-09 18:47:32 +0530

    added 75

--- src/Arrays/Example75.java
+++ src/Arrays/Example75.java
@@ -2,10 +2,12 @@ package Arrays;
 
 class Example75 {
     //maximum total subarray value
-    static long maxTotalValue(int[] nums, int k) {
+    static long maxTotalValue(int[] nums, int k)
+    {
         int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
 
-        for (int num : nums) {
+        for (int num : nums)
+        {
             min = Math.min(min, num);
             max = Math.max(max, num);
         }
@@ -13,7 +15,8 @@ class Example75 {
         return 1L * (max - min) * k;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         int[] arr = {1,3,2};
         int k =2;
         System.out.println(maxTotalValue(arr,k));
