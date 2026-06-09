67ff275da8d6a296379dfb9d9b8bbe7903902726
anuroop <anuroopkanade09@gmail.com>
2026-06-09 18:47:02 +0530

    added 75

---
+++ src/Arrays/Example75.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+class Example75 {
+
+    static long maxTotalValue(int[] nums, int k) {
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        return 1L * (max - min) * k;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,2};
+        int k =2;
+        System.out.println(maxTotalValue(arr,k));
+    }
+}
